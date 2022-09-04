9c067c9971e348ecdb9ab39333a94edcf3b01c32
Pavlo Bieliak <66524818+Paul48wt@users.noreply.github.com>
2022-09-04 18:48:58 +0300

    task-2 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const makeLi = text => {
+  const li = document.createElement('li');
+
+  li.textContent = text;
+  li.classList.add('item');
+  return li;
+};
+const array = ingredients.map(makeLi);
+
+const ul = document.querySelector('#ingredients');
+ul.append(...array);
