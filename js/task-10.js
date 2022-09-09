6d422613d89216b35c427f5d8ced0c6c07641af3
Pavlo Bieliak <66524818+Paul48wt@users.noreply.github.com>
2022-09-09 12:08:22 +0300

    task-10 done

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,32 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const input = document.querySelector('input');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxes = document.querySelector('#boxes');
+let amount = 0;
+function createBoxes(amount) {
+  let width = 30;
+  for (let i = 0; i < amount; i += 1) {
+    boxes.insertAdjacentHTML(
+      'beforeend',
+      `<div style="width: ${width}px; height: ${width}px; background-color: ${getRandomHexColor()}"></div>`,
+    );
+    width += 10;
+  }
+}
+
+input.addEventListener('input', event => {
+  amount = Number(event.currentTarget.value);
+});
+
+createBtn.addEventListener('click', () => {
+  createBoxes(amount);
+});
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
+
+destroyBtn.addEventListener('click', destroyBoxes);
